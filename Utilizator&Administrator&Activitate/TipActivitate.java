055ec39e8a0b40d2d1f79a84ce46af2aff75240b
CosmuleteCosmin <151380414+CosmuleteCosmin@users.noreply.github.com>
2026-04-04 20:48:28 +0300

    Create TipActivitate.java

---
+++ Utilizator&Administrator&Activitate/TipActivitate.java
@@ -0,0 +1,8 @@
+
+public enum TipActivitate {
+    TEREN,        // Ecologizare, plantare, distribuție ajutoare
+    LOGISTICA,    // Organizare depozit, transport
+    ADMINISTRATIV,// Introducere date, secretariat
+    FUNDRAISING,  // Colectare fonduri, evenimente caritabile
+    EDUCATIONAL   // Training-uri, meditații pentru copii
+}

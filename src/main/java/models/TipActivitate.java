ffe9a106439bcf1ff35a1fbd01c6d1febaf45f2c
Sova Ioan-Rares <147800335+sovarares@users.noreply.github.com>
2026-06-18 17:43:50 +0300

    Add files via upload

---
+++ src/main/java/models/TipActivitate.java
@@ -0,0 +1,9 @@
+package models;
+
+public enum TipActivitate {
+    TEREN,        // Ecologizare, plantare, distribuție ajutoare
+    LOGISTICA,    // Organizare depozit, transport
+    ADMINISTRATIV,// Introducere date, secretariat
+    FUNDRAISING,  // Colectare fonduri, evenimente caritabile
+    EDUCATIONAL   // Training-uri, meditații pentru copii
+}
